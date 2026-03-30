5af34bc08d256e6e8a5bf032829aba13093bdad5
RakaAlrian <57884963+RakaAlrian@users.noreply.github.com>
2026-03-30 09:13:20 +0700

    Update imports to use infinite_scroll_pagination_5

--- lib/src/base/paged_layout_builder.dart
+++ lib/src/base/paged_layout_builder.dart
@@ -2,16 +2,16 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:infinite_scroll_pagination/src/base/paged_child_builder_delegate.dart';
-import 'package:infinite_scroll_pagination/src/core/extensions.dart';
+import 'package:infinite_scroll_pagination_5/src/base/paged_child_builder_delegate.dart';
+import 'package:infinite_scroll_pagination_5/src/core/extensions.dart';
 
-import 'package:infinite_scroll_pagination/src/defaults/first_page_error_indicator.dart';
-import 'package:infinite_scroll_pagination/src/defaults/first_page_progress_indicator.dart';
-import 'package:infinite_scroll_pagination/src/defaults/new_page_error_indicator.dart';
-import 'package:infinite_scroll_pagination/src/defaults/new_page_progress_indicator.dart';
-import 'package:infinite_scroll_pagination/src/defaults/no_items_found_indicator.dart';
-import 'package:infinite_scroll_pagination/src/core/paging_state.dart';
-import 'package:infinite_scroll_pagination/src/core/paging_status.dart';
+import 'package:infinite_scroll_pagination_5/src/defaults/first_page_error_indicator.dart';
+import 'package:infinite_scroll_pagination_5/src/defaults/first_page_progress_indicator.dart';
+import 'package:infinite_scroll_pagination_5/src/defaults/new_page_error_indicator.dart';
+import 'package:infinite_scroll_pagination_5/src/defaults/new_page_progress_indicator.dart';
+import 'package:infinite_scroll_pagination_5/src/defaults/no_items_found_indicator.dart';
+import 'package:infinite_scroll_pagination_5/src/core/paging_state.dart';
+import 'package:infinite_scroll_pagination_5/src/core/paging_status.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
 /// Called to request a new page of data.
